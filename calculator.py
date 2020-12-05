dd4040c3a1b2cf0786e9ed008f86b207e45e2dd6
Jeong Unsan <74296888+tree0204@users.noreply.github.com>
2020-12-05 22:44:40 +0900

    Calculator

---
+++ calculator.py
@@ -0,0 +1,27 @@
+def plus(a, b):
+  return int(a) + int(b);
+
+def minus(a, b):
+  return int(a) - int(b);
+
+def time(a, b):
+  return int(a) * int(b);
+
+def divisions(a, b):
+  return int(a) / int(b);
+
+def nagation(a):
+  return -int(a);
+def power(a, b):
+  return int(a) ** int(b);
+
+def reminder(a, b):
+  return int(a) % int(b);
+
+print(plus(a, b))
+print(minus(a, b))
+print(time(a, b))
+print(divisions(a, b))
+print(nagation(a))
+print(power(a, b))
+print(reminder(a, b))
